93f9bef16d404e12468ea69db33766a3c07ef1a4
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-01-25 22:17:22 -0300

    Adicionando GrupoController

---
+++ SGCOM/SGCOM.Api/Controllers/GrupoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SGCOM.Domain;
+using SGCOM.Infra.DataContexts;
+
+namespace SGCOM.Api.Controllers
+{
+    [RoutePrefix("api/v1/public")]
+    public class GrupoController : ApiController
+    {
+        private SGComDataContext db = new SGComDataContext();
+
+        [Route("grupos")]
+        public HttpResponseMessage GetGrupos()
+        {
+            var result = db.Grupos.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("grupos/{grupoId}")]
+        public HttpResponseMessage GetGruposById(int grupoId)
+        {
+            var result = db.Grupos.Where(x => x.Id == grupoId).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [HttpPost]
+        [Route("grupos")]
+        public HttpResponseMessage PostGrupos(Grupo grupo)
+        {
+            if (grupo == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Grupos.Add(grupo);
+                db.SaveChanges();
+
+                var result = grupo;
+                return Request.CreateResponse(HttpStatusCode.Created, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,"Falha ao incluir grupo.");
+            }            
+        }
+
+        //Update
+        [HttpPatch]
+        [Route("grupos")]
+        public HttpResponseMessage PatchGrupo(Grupo grupo)
+        {
+            if (grupo == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Grupo>(grupo).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = grupo;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar grupo.");
+            }
+        }
+
+        //Update
+        [HttpPut]
+        [Route("grupos")]
+        public HttpResponseMessage PutGrupo(Grupo grupo)
+        {
+            if (grupo == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Grupo>(grupo).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = grupo;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar grupo.");
+            }
+        }
+
+        //Update
+        [HttpDelete]
+        [Route("grupos")]
+        public HttpResponseMessage DeleteGrupo(int grupoId)
+        {
+            if (grupoId < 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Grupos.Remove(db.Grupos.Find(grupoId));
+                db.SaveChanges();
+                                
+                return Request.CreateResponse(HttpStatusCode.OK, "Produto excluído");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar grupo.");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {                        
+            db.Dispose();            
+            base.Dispose(disposing);
+        }       
+
+    }
+}
