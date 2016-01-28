20ed8d2f17abbd2a937cbad64244c3b54a50f75c
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-01-28 01:06:02 -0300

    Criação Classe Caixa

---
+++ SGCOM/SGCOM.Models/Caixa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SGCOM.Models
+{
+    public class Caixa
+    {
+        public Caixa()
+        {
+            this.DataAbertura = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public int NumeroCaixa { get; set; }
+        public decimal ValorAbertura { get; set; }
+        public decimal ValorFechamento { get; set; }
+        public decimal ValorSangria { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public DateTime DataFechamento { get; set; }        
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
