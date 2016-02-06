ee950084720d7fd944671883b1622f3cedf9f475
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-02-06 17:42:13 -0300

    Correção

--- SGCOM/SGCOM.Net/Controllers/HomeController.cs
+++ SGCOM/SGCOM.Net/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SGCOM.WebApplication.Controllers
+namespace SGCOM.Net.Controllers
 {
     public class HomeController : Controller
     {
