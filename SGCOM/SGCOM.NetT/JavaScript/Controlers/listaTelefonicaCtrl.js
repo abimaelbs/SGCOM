100cf99c0e780678c50536b790caf2529080fdd0
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-02-18 02:14:10 -0300

    Correção estrutura projeto

---
+++ SGCOM/SGCOM.NetT/JavaScript/Controlers/listaTelefonicaCtrl.js
@@ -0,0 +1,47 @@
+﻿angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+    $scope.operadoras = [
+        { nome: "OI", codigo: 14, categoria: "Celular", preco: 2 },
+        { nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1 },
+        { nome: "Tim", codigo: 41, categoria: "Celula", preco: 3 },
+        { nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1 },
+        { nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2 }
+    ];
+
+    var carregarContatos = function () {
+        $http.get("http://localhost:52055/api/public/listaTelefonicas").success(function (data) {
+            $scope.contatos = data;
+        }).error(function (data, sstatus) {
+            $scope.message = "Erro ao retornoar clientes: " + data;
+        });
+    };
+    $scope.adicionarContato = function (contato) {
+        $http.post("http://localhost:52055/api/public/listaTelefonicas", contato).success(function (data) {
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        });
+    };
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = this.contatos.filter(function (contato) {
+            //if (!contato.selecionado) return contato;
+            if (!contato.selecionado) {
+                $http.post("http://localhost:52055/api/public/listaTelefonicas", 6).success(function (data) {
+                    carregarContatos();
+                });
+            }
+        });
+    };
+    $scope.isContatoSelecionado = function (contatos) {
+        return this.contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioOrdenacao = campo;
+        $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
+    }
+
+    carregarContatos();
+});
