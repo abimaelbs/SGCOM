36cda22fab117d3d9a86d050a3562186a54a5457
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-03-03 02:00:11 -0400

    coreçãoo commit

---
+++ SGCOM/SGCOM.NetT/Scripts/Directives/uiAlertDirective.js
@@ -0,0 +1,11 @@
+﻿angular.module("listaTelefonica").directive("uiAlert", function () {
+    return {
+        templateUrl: "Shared/_Alert.html",
+        replace: true,
+        restrict: "AE",
+        scope: {
+            title: "@"
+        },
+        transclude:true
+    }
+});
