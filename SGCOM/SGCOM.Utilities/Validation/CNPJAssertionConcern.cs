08670d140e031128179f21db3d4317c4ef033858
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-03-20 13:52:49 -0400

    Add assertion

---
+++ SGCOM/SGCOM.Utilities/Validation/CNPJAssertionConcern.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCOM.Utilities.Validation
+{
+    public class CNPJAssertionConcern
+    {
+        private static bool Validate(string cnpj)
+        {
+            if (string.IsNullOrEmpty(cnpj))
+                return false;
+            int[] multiplier1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplier = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int sum;
+            int remainder;
+            string digit;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            sum = 0;
+            for (int i = 0; i < 12; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplier1[i];
+            remainder = (sum % 11);
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+            digit = remainder.ToString();
+            tempCnpj = tempCnpj + digit;
+            sum = 0;
+            for (int i = 0; i < 13; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplier[i];
+            remainder = (sum % 11);
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+            digit = digit + remainder.ToString();
+            return cnpj.EndsWith(digit);
+        }
+
+        public static void AssertCNPJFormat(string cnpj, string message)
+        {
+            if (!Validate(cnpj))
+                throw new InvalidOperationException(message);
+        }
+    }
+}
