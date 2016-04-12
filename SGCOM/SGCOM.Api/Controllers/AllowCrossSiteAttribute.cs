8035a90b96b7aed2a4f0b66930333f84a04074fa
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2016-04-12 11:03:29 -0400

    Erro permissão

---
+++ SGCOM/SGCOM.Api/Controllers/AllowCrossSiteAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace SGCOM.Api.Controllers
+{
+    public class AllowCrossSiteAttribute : System.Web.Mvc.ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            base.OnActionExecuted(filterContext);
+        }
+       
+    }
+}
